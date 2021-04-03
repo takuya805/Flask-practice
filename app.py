5f631f0ad3f968eec009e4caa593e1faac8c79f4
takuya805 <takuya0903exile@gmail.com>
2021-04-03 18:15:25 +0900

    create-edit

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class Post(db.Model):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.due).all()
         return render_template('index.html', posts=posts)
     else:
         title = request.form.get('title')
@@ -37,6 +37,35 @@ def index():
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+    return render_template('detail.html', post=post)
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/')
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
 
 
 if __name__ == '__main__':
