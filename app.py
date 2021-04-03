140f0bf1fc52754932f2f98b33dddb580e2cc272
takuya805 <takuya0903exile@gmail.com>
2021-04-03 17:16:49 +0900

    create task

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello World'
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
+
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
